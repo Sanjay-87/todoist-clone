b146199befdfa7deda1fa3fad383e3642d2157c3
Aditya Gupta <aditya.gupta@mountblue.tech>
2020-05-13 14:29:27 +0530

    update section and change style of button

--- src/reducers/sectionReducers.js
+++ src/reducers/sectionReducers.js
@@ -1,6 +1,7 @@
 import { FETCH_SECTIONS } from "../actions/actionTypes";
 import { DELETE_SECTIONS } from "../actions/actionTypes";
 import { INSERT_SECTIONS } from "../actions/actionTypes";
+import { UPDATE_SECTIONS } from "../actions/actionTypes";
 
 const initialState = { section: [] };
 
@@ -23,9 +24,26 @@ export default (state = initialState, action) => {
            return { ...state,
              section:[...state.section,action.payload] };
         }
+        case UPDATE_SECTIONS: {
+            return{
+                ...state,
+                section: [
+                    ...state.section.map(section=>{
+                        if(section.id !== action.payload.sectionId){
+                            return section;
+                        }
+                        else {
+                            const updateSection = section;
+                            updateSection.name = action.payload.name;
+                            return updateSection;
+                        }
+                    }),
+                ],
+            };
+
+        }
 
         default:
             return state;
     }
 };
-
